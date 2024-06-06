fe4c40bd4a0c1fa5c6832207f940f949faf4efa5
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-06 16:22:10 +0530

    Basic Operations & Indexing

---
+++ Chapter 2/02_Basic_Operators.py
@@ -0,0 +1,13 @@
+#TODO: Concatenation.
+
+str1 = "Hello"
+str2 = "World"
+
+print(str1 + str2)
+
+#TODO: Length of str
+
+str1 = "Hello"
+str2 = "World"
+
+print(len(str1))
---
+++ Chapter 2/02_Indexing.py
@@ -0,0 +1,8 @@
+str = "Code_With_Ssn"
+
+ch = str[0]
+print(ch)
+
+# OR
+
+# print(str[0])
