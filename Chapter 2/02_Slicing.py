a23574dafcd5bc1808ec42ed87e63f3e6d932c2f
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-06 17:01:58 +0530

    Slicing ANd Strings Function

---
+++ Chapter 2/02_Slicing.py
@@ -0,0 +1,12 @@
+#TODO: Slicing
+
+str ="Code_With_Ssn"
+
+print(str[4:8])
+print(str[:4])
+print(str[5:])
+
+#TODO: Negative Index 
+
+str = "Apple"
+print(str[-3:-1])
---
+++ Chapter 2/02_Strings_Function.py
@@ -0,0 +1,12 @@
+str = "I am studying python from ApnaColllege"
+print(str.endswith("ege"))
+
+print(str.capitalize()) #Creates new string 
+
+print(str.replace("o", "a"))
+
+print(str.find("o")) #also can write full word.
+
+print(str.count("from"))
+
+
