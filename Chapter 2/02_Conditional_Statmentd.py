db2a637e05ab9e450e34d05c42175afee8e855d9
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-07 15:40:52 +0530

    Started Conditional Statements

---
+++ Chapter 2/02_Conditional_Statmentd.py
@@ -0,0 +1,13 @@
+age = 21
+light = "green"
+if(age >= 18):
+    print("Can Vote and apply for license")
+
+    if(light == "red"):
+       print("Stop")
+
+    elif(light == "green"):
+        print("Go")
+
+    elif(light == "yellow"):
+        print("Slow Down")
