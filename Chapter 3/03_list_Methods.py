416ee61e214d2443024a46a851d3f121e03d0fff
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-08 12:14:53 +0530

    List Methods

---
+++ Chapter 3/03_list_Methods.py
@@ -0,0 +1,15 @@
+list = [2, 1, 3]
+
+#list.append(4)
+
+#print(list.sort())
+
+#print(list.sort(reverse=True))
+
+# print(list.reverse())  
+
+#list.insert(1, 10)
+
+#list.remove(1)
+list.pop(2)
+print(list)
