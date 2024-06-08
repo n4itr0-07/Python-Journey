e673525bdac93297184faecd4cbacf64fc6dea44
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-08 11:48:32 +0530

    List Slicing

--- Chapter 3/03_list_In_Python.py
+++ Chapter 3/03_list_In_Python.py
@@ -10,4 +10,17 @@
 
 marks = [94.5, 81.25, 72.8, 93.4, 76.8] #TODO: --> This is called list in python.
 print(marks)
+
 print(type(marks))
+
+print(len(marks))
+
+print(marks[4])
+
+#TODO: --> Below format is also allowed in python ⌂
+
+student = ["Atif", 99.8, 1, "Rajouri"]
+
+student[0] = "Salik"  #TODO: This is also allowed in python.
+
+print(student)
