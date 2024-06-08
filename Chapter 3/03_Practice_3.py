68b191f2110f0fb52c90e51c05981d442fec6e72
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-08 15:26:28 +0530

    Practice Question 3

---
+++ Chapter 3/03_Practice_3.py
@@ -0,0 +1,30 @@
+#TODO: Write a program to count the numbers of students with the "A" grade in the following tuple.
+
+# ["C", "D", "A", "A", "B", "B", "A"]
+# Store the above values in a list & sort them from "A" to "D".
+
+
+# grade = ("C", "D", "A", "A", "B", "B", "A")
+
+# print(grade.count("A"))
+
+#FIXME:  Part 2 of question below 
+
+grade = ["C", "D", "A", "A", "B", "B", "A"]
+grade.ort()
+print(grade)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
