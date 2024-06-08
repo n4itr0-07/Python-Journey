dbd01716800d925ac4769862d58db31d533987a2
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-08 15:05:18 +0530

    Added Practice Question 1

---
+++ Chapter 3/03_Practice_01.py
@@ -0,0 +1,15 @@
+#TODO: Write a program to ask the user to enter names of their 3 favorite movies and store them in a list.
+
+movies = []
+
+movie1 = input("Enter name of first movie ->")
+movie2 = input("Enter name of second movie ->")
+movie3 = input("Enter name of third movie ->")
+
+movies.append(movie1)
+
+movies.append(movie2)
+
+movies.append(movie3)
+
+print(movies)
