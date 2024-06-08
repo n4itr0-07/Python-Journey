9dff16d8fc9aeedcc13aff79c18f9b972107765b
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-08 11:27:28 +0530

    Started Chapter 3

---
+++ Chapter 3/03_list_In_Python.py
@@ -0,0 +1,13 @@
+# marks1 = 94.5
+
+# marks2 = 81.25
+
+# marks3 = 72.8
+
+# marks4 = 93.4
+
+# marks5 = 76.8
+
+marks = [94.5, 81.25, 72.8, 93.4, 76.8] #TODO: --> This is called list in python.
+print(marks)
+print(type(marks))
