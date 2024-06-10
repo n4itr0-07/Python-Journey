04108d599e539b344c2d32e49cf14ed420542f95
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-10 12:03:58 +0530

    Updated Practice Question 1

---
+++ Chapter 4/04_Practice_1.py
@@ -0,0 +1,14 @@
+#TODO: Store the following words meaning in python dictionary :
+
+# table : "a piece of furniture", "list of facts & figures"
+
+# cat : "a small animal"
+
+
+dictionary = {
+    "cat": "a small animal",
+    "table": ["a piece of furniture", "list of facts & figures"]
+
+}
+
+print(dictionary)
