211d19695c56c3fd20cf76beb0443dc710875760
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-10 09:49:18 +0530

    Sets Method In Python

--- Chapter 4/04_Set_Methods.py
+++ Chapter 4/04_Set_Methods.py
@@ -2,3 +2,44 @@
 
 # sets elements --> Immutable
 
+collection = set()
+
+collection.add(10)
+
+collection.add(20)          # --> Add Methods.
+
+collection.add(30)
+
+collection.add("Modi 🤡")  # --> String value
+
+collection.add((1, 2, 3, 4))  # --> Tuple value
+
+#TODO: collection.add([1, 2, 3, 4]) # --> List value but in set we can not add list value it will show error.
+
+collection.remove(10)  # --> Remove Method.
+
+collection.clear() # --> Clear Method.
+
+print(collection)
+
+print(len(collection))
+
+#TODO: For POP Method Check Below Code :-
+
+set = {"Delhi", "world", "Coding", "Python"}
+
+print(set.pop()) # POP Method.
+
+#TODO: For Union Method Check Below Code :-
+
+set1 = {1, 2, 3}
+set2 = {2, 3, 4}
+
+print(set1.union(set2)) # Union Method.
+
+#TODO: For Intersection Method Check Below Code :-
+
+set1 = {1, 2, 3}
+set2 = {2, 3, 4}
+
+print(set1.intersection(set2)) # Intersection Method.
