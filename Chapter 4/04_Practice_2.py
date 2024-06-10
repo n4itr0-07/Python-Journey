758d05a1c80414084b9fba38257ffaf3530b5f11
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-10 12:16:35 +0530

    Updated Practice Question 2

---
+++ Chapter 4/04_Practice_2.py
@@ -0,0 +1,10 @@
+#TODO: You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many  classrooms are required by all students.
+
+# "python", "java", "C++", "python", 'javascript", "java", "python", "C++", "C" 
+
+subjects = {
+    "python", "java", "C++", "python", 'javascript", "java","python", "c++", "C"
+}
+
+print(subjects)
+print(len(subjects))
