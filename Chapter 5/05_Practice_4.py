70c55dc03c1a684301ed79a9f2fe0891fa8ecb0a
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-16 15:32:29 +0530

    Chapter 5 Practice Set Question 4

---
+++ Chapter 5/05_Practice_4.py
@@ -0,0 +1,9 @@
+
+#TODO: Print the elements of the following list using a loop : [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
