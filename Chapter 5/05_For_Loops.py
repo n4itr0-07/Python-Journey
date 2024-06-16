d7b23517b75384aa61afeafb9cefa99dfd90bf1f
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-16 18:14:44 +0530

    For loops In Python

---
+++ Chapter 5/05_For_Loops.py
@@ -0,0 +1,12 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for val in nums:
+    print(val)
+    print("\n ********************************************************")
+
+    #TODO: Using for loop in tuples.
+
+    tup = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+for num in tup:
+    print(num)
