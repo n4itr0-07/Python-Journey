ec048bc533cde435814c8bcc437d9c3bebc4ae9a
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-16 18:49:26 +0530

    rang()

---
+++ Chapter 5/05_range.py
@@ -0,0 +1,21 @@
+seq = range(10)
+
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
+#TODO: Method 1 
+# for i in seq:
+#     print(i)
+
+    #TODO: Method 2 of writing range
+
+# for i in range(2, 10):  # range(start, stop)
+#     print(i)  
+
+    #TODO: Method 3 of writing range
+
+for i in range(2, 10 , 2): # range(start, stop, step)
+    print(i)
+
