9a3657700df3a14e2c90d3c5722fcc3dda117c6a
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-16 14:50:55 +0530

    While loop In Python Chapter 1

---
+++ Chapter 5/05_While_Loop.py
@@ -0,0 +1,17 @@
+# count = 1
+
+# while count <= 5:
+#     print("Hello World", count)
+
+#     count += 1
+
+    # print(count)
+
+    #TODO: Prime Number from 1 to 100
+
+i = 1
+while i <= 100:
+        print(i)
+        i +=1
+
+        print("Loop Ended")
