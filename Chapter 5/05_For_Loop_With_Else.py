50d498309994e7d103a5c2a78d90cda7799e7030
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-16 18:20:47 +0530

    For loops With Else In Python

---
+++ Chapter 5/05_For_Loop_With_Else.py
@@ -0,0 +1,10 @@
+str = "Google Inc."
+
+for char in str:
+    if(char == 'o'):
+        print("o Found")
+        break
+    print(char)
+
+else:
+    print("Done")
--- Chapter 5/05_For_Loops.py
+++ Chapter 5/05_For_Loops.py
@@ -2,7 +2,9 @@ nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for val in nums:
     print(val)
+
     print("\n ********************************************************")
+    
 
     #TODO: Using for loop in tuples.
 
