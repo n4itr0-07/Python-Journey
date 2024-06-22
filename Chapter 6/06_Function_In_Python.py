3e854b01a1889eebf64bd0589d8dd3d113832478
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-22 10:07:09 +0530

    Chapter 6 Functions In Python

---
+++ Chapter 6/06_Function_In_Python.py
@@ -0,0 +1,20 @@
+
+#TODO:  Function Definition
+def calcSum(a, b): #TODO:  Parameters
+    sum = a + b
+    print(sum)
+    return sum
+
+calcSum(10, 20) #TODO:  Function Call ; and 10, 20 are arguments.
+calcSum(12, 30)
+calcSum(31, 40)
+
+
+def print_Hello():
+    print("Hello")
+
+print_Hello()
+print_Hello()
+print_Hello()
+print_Hello()
+print_Hello()
