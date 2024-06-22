ac6e9883b5ec85a4914fb3855dc4e02e9b5bbe0c
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-22 10:36:08 +0530

    Chapter 6 Practice Question 2

---
+++ Chapter 6/06_Practice_2.py
@@ -0,0 +1,15 @@
+
+#TODO: Write a function to print the elements of a list in a single line. (list is the parameter)
+
+
+cities = ["Kashmir", "Jammu", "Rajouri", "Pounch"]
+
+states = ["Jammu & Kashmir", "Delhi", "Mumbai", "Punjab", "Uttar Pradesh","Bihar", "Kolkata"]
+
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
+
+
+print_list(cities)
+print()
