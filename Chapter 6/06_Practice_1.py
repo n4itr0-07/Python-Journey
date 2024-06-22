381bc142d54b0654e3a8ed50293e6bf80693da0e
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-22 10:29:40 +0530

    Chapter 6 Practice Question 1

---
+++ Chapter 6/06_Practice_1.py
@@ -0,0 +1,13 @@
+
+#TODO: Write a function to print the length of a list. (list is the parameter)
+
+cities = ["Kashmir", "Jammu", "Rajouri", "Pounch"]
+
+states = ["Jammu & Kashmir", "Delhi", "Mumbai", "Punjab", "Uttar Pradesh","Bihar", "Kolkata"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+
+print_len(states)
