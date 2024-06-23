4e69f35003ce864220bfdb203291da658954dc6c
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-23 09:21:25 +0530

    File Write Mode

--- Chapter 7/07_File.py
+++ Chapter 7/07_File.py
@@ -1,4 +1,4 @@
-f = open("demo.txt", "r")
+f = open("C:\Code With Ssn\Learning Python\Python-Journey\Chapter 7\demo.txt", "r")
 data = f.read()
 print(data)
 print(type(data))
---
+++ Chapter 7/07_write.py
@@ -0,0 +1,4 @@
+f = open("C:\Code With Ssn\Learning Python\Python-Journey\Chapter 7\demo.txt", "w")
+
+f.write(" Hy my self salik seraj naik")
+f.close()
