d3ec985f98574b27f4ed02fe5e51dedbbf532e78
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-23 09:37:22 +0530

    Practice 1

---
+++ Chapter 7/07_Practice_1.py
@@ -0,0 +1,3 @@
+with open("C:\Code With Ssn\Learning Python\Python-Journey\Chapter 7\Practice1.txt", "w") as f:
+    f.write("I am studying Python check my github repo\n")
+    f.write("Thanks for checking my github repo")
