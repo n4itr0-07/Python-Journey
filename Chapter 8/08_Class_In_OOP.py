3c0f746fb38ea2f048cbd226aacaf54eef2c8e2f
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-26 08:26:14 +0530

    Started Chapter 8 OOP

---
+++ Chapter 8/08_Class_In_OOP.py
@@ -0,0 +1,18 @@
+
+#TODO: Creating Class
+class student:
+    name = "Salik"
+
+
+#TODO: Creating object (instance)
+
+s1 = student()
+print(s1.name)
+
+
+
+class Car:
+    color = "green"
+
+car1 = Car()
+print(car1.color)
