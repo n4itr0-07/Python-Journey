1a377aed00b718b46995ceed8c7d7449f3945c42
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-02 19:24:37 +0530

    Practice Question Solution 5

---
+++ Chapter 10/05_solution.py
@@ -0,0 +1,12 @@
+Weather = str(input("Enter Weather Condition ->"))
+
+if Weather == "Sunny":
+    print("It's a great day to go for hiking or swimming! 🏊‍♂️, or Go for a walk")
+
+elif Weather == "Rainy":
+    print("Don't forget your umbrella! , Stay hydrated and enjoy the rain ☔,  Also You can Read a book")
+
+else:
+    print("It's a pleasant day to enjoy outdoor activities!")
+    import sys
+    sys.exit()
