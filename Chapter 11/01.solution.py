9746d896c60e60a5e88cfb4afdf58389a1cbbb70
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-04 08:41:18 +0530

    Chapter 11 Loops Question Solution 1

---
+++ Chapter 11/01.solution.py
@@ -0,0 +1,11 @@
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+
+positive_number_count = 0
+
+for num in numbers:
+    if num > 0:
+        positive_number_count += 1
+
+print("Final count of Positive Numbers is ->", positive_number_count)
+
+
