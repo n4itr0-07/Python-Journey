3e1b7750508d79e179804e4851e9c1a53c58316b
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-05 16:50:02 +0530

    Chapter 12 Functions Question 8 Solution

---
+++ Chapter 12/08_solution.py
@@ -0,0 +1,11 @@
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+#     print("Name ", name, "Power ", power)
+
+
+print_kwargs(name="Salik", power="iman")
+print_kwargs(name="Salik", power="iman")
+print_kwargs(name="Salik", power="iman")
+print_kwargs(name="Salik", power="iman", enemy = "Non Believers")
