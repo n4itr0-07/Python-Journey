4390e18c7b93f59501ba69fbbe9f2bb0b7494dd4
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-06 14:39:10 +0530

    Chapter 13 | OOPS | Solution 2

--- Chapter 13/02_solution.py
+++ Chapter 13/02_solution.py
@@ -5,6 +5,12 @@ class Car:
         self.brand = brand
         self.model = model
 
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+
 my_car = Car("Toyota", "Lamborghini")
 print(my_car.brand)
 print(my_car.model)
+
+print(my_car.full_name())
