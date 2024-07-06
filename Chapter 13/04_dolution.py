c6930f3861664c6dcd9eaef239906aa8c334f5bb
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-06 14:57:12 +0530

    Chapter 13 | OOPS | Solution 4

---
+++ Chapter 13/04_dolution.py
@@ -0,0 +1,34 @@
+class Car:
+    # __brand = None
+    # __models = None
+    def __init__(self, brand, models):
+        self.__brand = brand
+        self.models = models
+
+    def get_brand(self):
+        return self.__brand + " !"
+
+    def full_name(self):
+        return f"{self.__brand} {self.models}"
+    
+
+class ElectricCar(Car):
+    def __init__(self, __brand, models, battery_capacity):
+        super().__init__(__brand, models)
+        self.battery_capacity = battery_capacity
+
+
+my_tesla = ElectricCar("tesla", "models S", "85kwh")
+
+print(my_tesla.full_name())
+
+
+
+my_car = Car("Toyota", "Lamborghini")
+# print(my_car.__brand)
+# print(my_car.models)
+
+# print(my_car.full_name())
+
+# print(my_tesla.full_name())
+print(my_tesla.get_brand())
