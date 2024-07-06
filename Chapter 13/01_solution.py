81e4b97a9d6a9086495c1a4bd8ab93ee8d30ab79
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-06 14:24:11 +0530

    Chapter 13 | OOPS | Solution 1

--- Chapter 13/01_solution.py
+++ Chapter 13/01_solution.py
@@ -1,5 +1,11 @@
 class Car:
-    brand = None
-    model = None
+    # brand = None
+    # model = None
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
-my_car = Car()
+my_car = Car("Toyota", "Lamborghini")
+print(my_car.brand)
+print(my_car.model)
+
