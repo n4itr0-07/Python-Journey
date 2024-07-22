d6b7a5a500ff8b50a6b5d89f86217733be959c4f
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-22 19:27:47 +0530

    Project 3 | Text Editor App

---
+++ Mini Projects/03_Text_Editor.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+def new_file():
+    text.delete(1.0, tk.END)
+
+def open_file():
+    file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("Text File", "*.txt")])
+    if file_path:
+        with open(file_path, 'r') as file:
+            text.delete(1.0, tk.END)
+            text.insert(tk.END, file.read())
+
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text File", "*.txt")])
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(text.get(1.0, tk.END))
+            messagebox.showinfo("Save File", "File saved successfully")
+
+root = tk.Tk()
+root.title("Simple Text Editor")
+root.geometry("800x600")
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+
+text = tk.Text(root, wrap=tk.WORD, font=("Helvetica", 12), fg="blue")
+text.pack(expand=tk.YES, fill=tk.BOTH)
+
+root.mainloop()
